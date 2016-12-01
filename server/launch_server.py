8f19efec21b4a56f6d4bbbbaaa4d01f35e89198e
fikipollo <rafahd87@gmail.com>
2016-12-01 09:04:57 -0300

    Some small changes

--- server/launch_server.py
+++ server/launch_server.py
@@ -4,7 +4,7 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../")
 os.chdir(os.path.dirname(os.path.realpath(__file__)) + "/../")
 
-from server import Application
+from server.server import Application
 
 application = Application()
 app = application.app
