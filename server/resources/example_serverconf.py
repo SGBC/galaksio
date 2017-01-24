ae51e107cce0a481d7d4b869602fd7b748a2cc27
fikipollo <rafahd87@gmail.com>
2017-01-24 09:10:36 +0100

    Bug fixing

--- server/resources/example_serverconf.py
+++ server/resources/example_serverconf.py
@@ -4,7 +4,7 @@ SERVER_PORT_NUMBER        = 8081 ##THE PORT NUMBER THAT GALAKSIO LISTENS FOR REQ
 SERVER_ALLOW_DEBUG        = False ##ENABLE DEBUG, THIS OPTIOS IS JUST FOR DEVELOPMENT
 SERVER_SUBDOMAIN          = "" ##USE THIS OPTION IF GALAKSIO RUNS UNDER AN SPECIFIC SUBDOMAIN, E.G. myserver.com/galaksio (w/o proxy)
 SERVER_MAX_CONTENT_LENGTH = 20 ##THE MAX SIZE FOR THE REQUESTS SENT BY THE CLIENTS, IN MB
-ADMIN_ACCOUNTS            = "myadmin@account.com" ##ACCOUNTS THAT HAVE ADMINISTRATION RIGHTS, SEPARATED BY COMMAS
+ADMIN_ACCOUNTS            = "myadmin@galaksio.com" ##ACCOUNTS THAT HAVE ADMINISTRATION RIGHTS, SEPARATED BY COMMAS
 
 #FILES SETTINGS
 ROOT_DIRECTORY            = "" ##THE LOCATION FOR THE GALAKSIO FILES, LEAVE BLANK TO AUTO DETECT
@@ -22,4 +22,4 @@ smtp_secure               = "" ##Use tls, etc.
 smpt_username             = "notifications@mydomain.com" ##THE SENDER EMAIL, DEPENDS ON THE SMTP SETTINGS
 smpt_pass                 = "09bf93aae4166cd12775c2592a1c613c" ##THE SENDER PASS IN MD5 CODIFICATION, DEPENDS ON THE SMTP SETTINGS
 smpt_sender               = "notifications@mydomain.com" ##Sender email (From value at the email)
-smpt_sender_name          = "B3Galaxy" ##Sender name (From value at the email)
+smpt_sender_name          = "Galaksio" ##Sender name (From value at the email)
