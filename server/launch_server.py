5319372a14cfdd1a9f6fc01d425e36407bb8d18e
fikipollo <rafahd87@gmail.com>
2017-01-30 10:30:07 +0100

    small fix

--- server/launch_server.py
+++ server/launch_server.py
@@ -9,7 +9,7 @@ if not os.path.isfile(os.path.dirname(os.path.realpath(__file__)) + "/conf/serve
     import shutil
     conf_dir = os.path.dirname(os.path.realpath(__file__)) + "/conf/"
     res_dir = os.path.dirname(os.path.realpath(__file__)) + "/resources/"
-    shutil.copyfile(res_dir + "example_serverconf.py", conf_dir + "__init__.py")
+    shutil.copyfile(res_dir + "__init__.py", conf_dir + "__init__.py")
     shutil.copyfile(res_dir + "example_serverconf.py", conf_dir + "serverconf.py")
     shutil.copyfile(res_dir + "logging.cfg", conf_dir + "logging.cfg")
     isFirstLaunch = True
